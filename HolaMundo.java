8ebac03f376cc2e682bf51f6dddb4bd3d360c905
Jesus OJ <jojfreelance@gmail.com>
2020-01-07 11:03:33 -0600

    primeros programas en el curso de java

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/*
+* primer programa HolaMundo.java
+*/
+// Comentario de una sola linea
+public class HolaMundo{
+	public static void main (String args[]) {
+		System.out.println("¡Hola mundo! mi primer programa en java");
+	}
+}
